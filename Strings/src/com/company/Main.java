57f53c1857a231f345271a2cb88b86dcb889ac5f
Askhat <aserikov@rib.kg>
2019-01-10 13:56:49 +0600

    First commit

---
+++ Strings/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите полное имя ");
+
+        String st = sc.nextLine();
+
+        String[] array = st.split(" ");
+        for(String s : array){
+            System.out.println(s);
+        }
+
+        System.out.println(array[1].charAt(0)+". "+ array[2].charAt(0)+". "+ array[0]);
+
+    }
+}
